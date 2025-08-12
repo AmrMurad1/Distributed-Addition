1bc83d0107da90df03eaa628927e31a370a66efe
Amr Murad <amrmurad2003@gmail.com>
2025-08-12 19:13:25 +0300

    the gRPC service-A implemented

---
+++ service-A/internal/server/server.go
@@ -0,0 +1,30 @@
+package server
+
+import (
+	"context"
+	"log"
+
+	pb "service-A/internal/proto/addition"
+)
+
+// Server implements the additionService
+type Server struct {
+	pb.UnimplementedAdditionServiceServer
+}
+
+func NewServer() *Server {
+	return &Server{}
+}
+
+func (s *Server) Add(ctx context.Context, req *pb.AddRequest) (*pb.AddResponse, error) {
+	log.Printf("Received Add request: %d + %d", req.GetNum1(), req.GetNum2())
+
+	result := int64(req.GetNum1() + req.GetNum2())
+
+	response := &pb.AddResponse{
+		Result: result,
+	}
+
+	log.Printf("Sending Add response: %d", result)
+	return response, nil
+}
--- service-A/main.go
+++ service-A/main.go
@@ -1,21 +1,36 @@
 package main
 
 import (
-  "fmt"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+
+	pb "service-A/internal/proto/addition"
+	"service-A/internal/server"
 )
 
-//TIP <p>To run your code, right-click the code and select <b>Run</b>.</p> <p>Alternatively, click
-// the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.</p>
+const (
+	port = ":50051"
+)
 
 func main() {
-  //TIP <p>Press <shortcut actionId="ShowIntentionActions"/> when your caret is at the underlined text
-  // to see how GoLand suggests fixing the warning.</p><p>Alternatively, if available, click the lightbulb to view possible fixes.</p>
-  s := "gopher"
-  fmt.Printf("Hello and welcome, %s!\n", s)
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
 
-  for i := 1; i <= 5; i++ {
-	//TIP <p>To start your debugging session, right-click your code in the editor and select the Debug option.</p> <p>We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-	// for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.</p>
-	fmt.Println("i =", 100/i)
-  }
+	s := grpc.NewServer()
+
+	additionServer := server.NewServer()
+	pb.RegisterAdditionServiceServer(s, additionServer)
+
+	reflection.Register(s)
+
+	log.Printf("gRPC server listening at %v", lis.Addr())
+
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
 }
